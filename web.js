ad3f33be47b956514be9a82abfa8454560159e84
maxmargolin <margolinmax@gmail.com>
2018-04-21 13:56:49 +0300

    dynamic pics,links

--- web.js
+++ web.js
@@ -6,73 +6,43 @@ window.onload = function() {
         function loading() {
                 search.classList.add('loading');
 
-                setTimeout(function() {
-                        search.classList.remove('loading');
-                }, 1000);
 
 
 
 
-                // Create the XHR object.
-                function createCORSRequest(method, url) {
-                        var xhr = new XMLHttpRequest();
-                        if ("withCredentials" in xhr) {
-                                // XHR for Chrome/Firefox/Opera/Safari.
-                                xhr.open(method, url, true);
-                        } else if (typeof XDomainRequest != "undefined") {
-                                // XDomainRequest for IE.
-                                xhr = new XDomainRequest();
-                                xhr.open(method, url);
-                        } else {
-                                // CORS not supported.
-                                xhr = null;
-                        }
-                        return xhr;
-                }
 
-                // Helper method to parse the title tag from the response.
+                var searched = document.getElementById("input").value;
+                $.getJSON('http://165.227.172.221:3222/' + searched, function(data) {
+                        var names = document.getElementsByClassName("centered");
+                        var n;
+                        for (n = 0; n < names.length && data[n]!=undefined ; n++) {
+                                names[n].innerHTML = data[n]["recipeHeaderTitle"];
 
-
-                // Make the actual CORS request.
-                function makeCorsRequest() {
-                        // This is a sample server that supports CORS.
-                        searched=document.getElementById("input").value;
-                        var url = 'http://165.227.172.221:3222/'+searched;
-
-                        var xhr = createCORSRequest('GET', url);
-                        if (!xhr) {
-                                alert('CORS not supported');
-                                return;
                         }
 
-                        // Response handlers.
-
-                        xhr.onload = function() {
-                                var json = JSON.parse(xhr.responseText);
-                                var title = json[0]["recipeHeaderTitle"];
-                                var names = document.getElementsByClassName("centered");
-                                var n;
-                                for (n = 0; n < names.length; n++) {
-                                        names[n].innerHTML=json[n]["recipeHeaderTitle"];;
-                                }
-                        };
-
-                        xhr.onerror = function() {
-                                alert('Woops, there was an error making the request.');
-                        };
-
-                        xhr.send();
-                }
-                makeCorsRequest();
 
 
 
+                        var items = document.getElementsByClassName("item");
+                        var i;
+                        //reappear
+                        for (i = 0; i < items.length; i++) {
+                                items[i].classList.remove('fade');
+                                var sub = items[i].getElementsByTagName("a")[0].href=data[i]["recipeHeaderLink"];
+                                var sub = items[i].getElementsByTagName("img")[0].src=data[i]["recipeThumbnailURL"];
+                                setTimeout(function(item) {
+                                        item.classList.add('fade');
+                                }, 200, items[i]);
 
-                var items = document.getElementsByClassName("item");
-                var i;
-                for (i = 0; i < items.length; i++) {
-                        items[i].classList.toggle('fade');
-                }
+                        }
+
+                        //temporary to show cute search loadin
+                        setTimeout(function() {
+                                search.classList.remove('loading');
+                        }, 500);
+                });
+
+
         }
 
         button.addEventListener('click', loading);
